1a9274bcf02e7f71aade060f946abf140fc0dd14
Masoud Sarabadani <codewithkael@gmail.com>
2026-05-25 14:39:00 +0330

    Upgrade compileSdk to 37 and Coroutines to 1.11.0

--- ai-video-processor/build.gradle.kts
+++ ai-video-processor/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("com.google.mlkit:segmentation-selfie:16.0.0-beta3")
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.11.0")
 
 }
